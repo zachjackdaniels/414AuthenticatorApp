0bd7907fe059047ec39ad5d0aa6eb278738f1f18
Zachary Daniels <Zachary Daniels@DESKTOP-AMDA1IF>
2021-02-23 11:25:11 -0700

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using _414AuthenticationAppFinal.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(_414AuthenticationAppFinal.Areas.Identity.IdentityHostingStartup))]
+namespace _414AuthenticationAppFinal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
